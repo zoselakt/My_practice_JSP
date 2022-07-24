189b45b26131b86c0c7612456deb1ec1f7593e49
zoselakt <95839182+zoselakt@users.noreply.github.com>
2022-07-24 13:36:10 +0900

    Delete com.arang.medici directory

--- com.arang.medici/src/main/java/jv300/mod002/list_contoller/BoardCommand.java
+++
@@ -1,8 +0,0 @@
-package jv300.mod002.list_contoller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BoardCommand {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
